d010246ff2ba18864f162f36ae764dd0cddf04dd
Thiago Gonçalves <mg.az.ul@hotmail.com>
2022-07-17 17:08:46 -0300

    Date update

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -18,10 +18,14 @@ function SalesCard() {
 
     const [sales, setSales] = useState<Sale[]>([])
     useEffect(() => {
-        axios.get(`${BASE_URL}/sales`).then(response => {
+
+        const dmin = new Date(minDate).toISOString().slice(0, 10);        
+        const dmax = new Date(maxDate).toISOString().slice(0, 10);
+
+        axios.get(`${BASE_URL}/sales?minDate=${dmin}&maxDate=${dmax}`).then(response => {
             setSales(response.data.content);
         })
-    }, []);
+    }, [minDate, maxDate]);
     
     return(
     <>
